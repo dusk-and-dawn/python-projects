23fd748647f24a7269b85a39b41d4099f25e2234
dusk-and-dawn <141350702+dusk-and-dawn@users.noreply.github.com>
2023-08-04 13:36:52 +0200

    new approach

--- wa_analysis_clean.py
+++ wa_analysis_clean.py
@@ -3,23 +3,28 @@ lines = []
 names_lst = []
 lines_by_user = {}
 
+def wa_analysis(filepath, *name):
+    return filereading(filepath), naming(*name)
+
 def filereading(filepath):
     file = open(filepath, 'r')
     lines = file.readlines()
     file.close()
-    return lines
-# def naming(*name):
-#     for x in name:
-#         names_lst.append(x)
 
-# def store_lines(names_lst):
-#     for i in names_lst:
-#         lines_by_user[i] = 0
-#     for i in names_lst: 
-#         for x in lines:
-#             if i in x:
-#                 lines_by_user[i].append(x)
+def naming(*name):
+    for x in name:
+        names_lst.append(x)
+    return store_lines(names_lst)    
 
-print(filereading('/home/curiosity/python projects/_chat_cosmic.txt'))
-# def wa_analysis(filepath, *name):
+def store_lines(names_lst):
+    for i in names_lst:
+        lines_by_user[i] = 0
+    for i in names_lst: 
+        for x in lines:
+            if i in x:
+                lines_by_user[i].append(x)
+
+print(lines_by_user)
+wa_analysis('/home/plotty/Downloads/_chat_cosmic.txt', 'Kathrin', 'David', 'Kochanie', 'Jh'))
+
 
